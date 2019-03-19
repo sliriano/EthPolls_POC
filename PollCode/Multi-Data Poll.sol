9c37b30aed617cdeab5af426f3bff161b6b6a7b7
Steve <41870969+sliriano@users.noreply.github.com>
2019-03-19 11:33:39 -0400

    Update Multi-Data Poll Code
    Update the Multi-Data Poll Code by removing redundant showOptionResults functions and creating one base function that completes the same task. This function was modeled off of the showOptions function.

--- PollCode/Multi-Data Poll.sol
+++ PollCode/Multi-Data Poll.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.24;
+pragma solidity >=0.4.22 <0.6.0;
 
 contract Polls {
     
@@ -12,22 +12,22 @@ contract Polls {
         string option5;
         address[] voters; // list of people who have voted
         string[] optionList; // list of the options
-        uint[] voteResults;
+        uint[] voteResults; // array of results
         mapping(address=>bool) hasVoted; // keeps track of who has voted
         
     }
 
     mapping(address=>Poll) public polls; // creator => poll
     
-    function createPoll(string description,string option1, string option2, string option3, string option4, string option5) public returns(string) {
-        address[] filler; //filler array passed into the poll
-        string[] optionList; //also a filler
-        uint[] fill;
+    function createPoll(string memory description,string memory option1, string memory option2, string memory option3, string memory option4, string memory option5) public returns(bool) {
+        address[] memory voters; //initialize array of voters
+        string[] memory optionList; //initialize list of voting choices 
+        uint[] memory voteResults; // initialize array of voting results
         if (_checkPoll(msg.sender)) { // if user has a poll then..
-            return "You must destroy your other poll in order to create a new one";
+            return false;
         }
         else {
-            polls[msg.sender] = Poll(description,option1,option2,option3,option4,option5,filler,optionList,fill); // create poll
+            polls[msg.sender] = Poll(description,option1,option2,option3,option4,option5,voters,optionList,voteResults); // create poll
             //add options to the optionList
             polls[msg.sender].optionList.push(option1);
             polls[msg.sender].optionList.push(option2);
@@ -37,7 +37,7 @@ contract Polls {
             for(uint x =0; x< 5; x++) {
                 polls[msg.sender].voteResults.push(0);
             }
-            return "Creation complete";
+            return true;
         }
     }
     
@@ -48,19 +48,19 @@ contract Polls {
         return false;
     }
     
-    function vote(address creatorAddress, uint decision) public returns(string) {
+    function vote(address creatorAddress, uint decision) public returns(bool) {
         if(_checkVoter(msg.sender,creatorAddress) == false) { // if the user has not yet voted
             if (decision> 5 || decision <=0){
-                return "Invalid Decision";
+                return false;
             }
             else {
                 polls[creatorAddress].voteResults[decision-1] +=1;
                 polls[creatorAddress].hasVoted[msg.sender] = true;
                 polls[creatorAddress].voters.push(msg.sender);
-                return "Success";
+                return true;
             }
         }
-        return "You voted already";
+        return false;
     }
 
     function _checkVoter(address _user, address creator) private view returns (bool) {
@@ -78,31 +78,15 @@ contract Polls {
         }
     }
     
-    function showOptions(address _user, uint option) public view returns(string){
+    function showOptions(address _user, uint option) public view returns(string memory){
         return polls[_user].optionList[option];
     }
     
-    //functions to display the results of each option
-    function showOptionOneResults(address _user) public view returns(uint) {
-        return polls[_user].voteResults[0];
+    function showOptionResults(address _user, uint option) public view returns (uint) {
+        return polls[_user].voteResults[option];
     }
     
-    function showOptionTwoResults(address _user) public view returns(uint) {
-        return polls[_user].voteResults[1];
-    }
-    
-    function showOptionThreeResults (address _user) public view returns(uint) {
-        return polls[_user].voteResults[2];
-    }
-    
-    function showOptionFourResults(address _user) public view returns(uint) {
-        return polls[_user].voteResults[3];
-    }
-    
-    function showOptionFiveResults(address _user) public view returns(uint) {
-        return polls[_user].voteResults[4];
-    }
-    function displayDescription (address _user) public view returns (string) {
+    function displayDescription (address _user) public view returns (string memory) {
         return polls[_user].description;
     }
 }

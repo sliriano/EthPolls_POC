c41ca2611656da226f263cb07ce0d3bc390a4f7b
Steve <41870969+sliriano@users.noreply.github.com>
2019-03-19 11:59:26 -0400

    Update yesNoPoll
    This enables update mainly focused on increasing efficiency and decreasing contract interaction cost. The voting function was redesigned and now uses the binary values 1 and 2 in order to determine votes instead of using strings.  Also fixed a security flaw that allowed users to cast invalid votes. This fix is also in the vote function and can be found on lines 43-44.

--- PollCode/yesNoPoll.sol
+++ PollCode/yesNoPoll.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.24;
+pragma solidity >=0.4.22 <0.6.0;
 
 contract yesNoPoll {
 
@@ -12,10 +12,10 @@ contract yesNoPoll {
 
     mapping(address=>Poll) polls;
 
-    function createPoll(string description) public returns(bool) {
-        address[] no; //fillers to pass into the creation of poll
-        address[] yes;
-        address[] total;
+    function createPoll(string memory description) public returns(bool) {
+        address[] memory no; //fillers to pass into the creation of poll
+        address[] memory yes;
+        address[] memory total;
         if (_checkPoll(msg.sender) == false) { // if user already has a poll then..
             return false;
         }
@@ -32,16 +32,20 @@ contract yesNoPoll {
         return false;
     }
 
-    function vote(address creatorAddress, string decision) public {
+    function vote(address creatorAddress, uint256 decision) public returns(bool) {
         if(_checkVoter(msg.sender,creatorAddress) == false) { // if the user has not yet voted
-            if (keccak256(decision) == keccak256("yes")) { // if user votes yes
-                polls[creatorAddress].yesVotes.push(msg.sender); // add user to array
+            if (decision == 1) { // if user votes yes
+                polls[creatorAddress].yesVotes.push(msg.sender); // add user to yes array
             }
-            else if (keccak256(decision) == keccak256("no")) {
-                polls[creatorAddress].noVotes.push(msg.sender);
+            else if (decision == 2) { // if user votes no
+                polls[creatorAddress].noVotes.push(msg.sender); // add user to no array
+            }
+            else if (decision != 1 && decision != 0){ // if user votes neither 0 or 1, return false
+                return false;
             }
             polls[creatorAddress].hasVoted[msg.sender] = true;
             polls[creatorAddress].totalVotes.push(msg.sender);
+            return true;
         }
     }
 
@@ -69,7 +73,7 @@ contract yesNoPoll {
         return polls[_user].yesVotes.length;
     }
     
-    function displayDescription(address _user) public view returns (string) {
+    function displayDescription(address _user) public view returns (string memory) {
         return polls[_user].description;
     }
 }
